cd7ca6d03b835124353764549a15d580785de25a
Zane Scheel <zane.scheel@gmail.com>
2022-01-08 14:09:09 -0600

    added 5-day cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,57 @@
 var apiKey = "70539c089a192ad51dae93112ad8a637";
-var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=Houston&units=imperial&appid=" + apiKey;
-var currentSection = $("#current");
 var city = "Houston";
+var latLon = [1, 2];
+var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+var currentSection = $("#current");
+var futureSection = $("#five-day");
 
 
 fetch(apiUrlCurrent).then(function(response){
     if (response.ok){
-        console.log(response);
         response.json().then(function(data){
             createCurrentWeather(data);
+            futureWeather(data.coord.lat, data.coord.lon)
         })
     }
 })
 
+function futureWeather(lat, lon) {
+    var apiUrlFuture = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely_hourly_alerts&units=imperial&appid=" + apiKey;
+    fetch(apiUrlFuture).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                futureCards(data);
+            })
+        }
+    })
+}
+
+function futureCards(data) {
+    for (i = 1; i < 6; i++) {
+        var date = getDateFuture(data, i);
+        var temp = data.daily[i].temp.day;
+        var wind = data.daily[i].wind_speed;
+        var icon = data.daily[i].weather[0].icon;
+        var humidity = data.daily[i].humidity;
+        var card = $("<div class='col rounded card-color m-3'> <h6 class='text-light'>" + date + "</h6> </div>");
+        var weatherIcon = $("<img class='max-width-20' src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
+        var tempStat = $("<p class='text-light card-text'>Temp: " + temp + "°F</p>");
+        var windStat = $("<p class='text-light card-text'>Wind: " + wind + " MPH</p>");
+        var humidityStat = $("<p class='text-light card-text'>Humidity: " + humidity + " %</p>");
+
+        card.append(weatherIcon, tempStat, windStat, humidityStat);
+        futureSection.append(card);
+    }
+
+}
+
+function getDateFuture(data, i) {
+    var unixTime = data.daily[i].dt;
+    var date = new Date(unixTime*1000);
+    var finalDate = date.toLocaleDateString("en-US");
+    return finalDate;
+}
 
 function getDate(data) {
     var date = new Date(data.dt*1000+(data.timezone*1000));
@@ -28,10 +67,11 @@ function createCurrentWeather(data) {
     var wind = data.wind.speed;
     var lat = data.coord.lat;
     var lon = data.coord.lon;
-    var dataLog = $("<h1>" + city + " (" + getDate(data) + ")<image src='http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'></image></h1>")
+    var dataLog = $("<h1>" + city + " (" + getDate(data) + ")<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'></h1>")
     var dataLogging = $("<p>Temp: " + temp + "°F</p> <p>Wind: " + wind + "MPH</p> <p>Humidity: " + humidity +" %</p>");
     currentSection.append(dataLog);
     currentSection.append(dataLogging);
+
     fetch("https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon).then(function(response){
         if(response.ok){
             response.json().then(function(data){
@@ -48,3 +88,4 @@ function createCurrentWeather(data) {
         }
     })
 }
+
